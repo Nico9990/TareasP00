a302887671b5ce9b58ecf3164cc629c2918abcdb
Nico9990 <nicolasguzman070@gmail.com>
2024-09-18 17:23:06 -0600

    Tarea no 9 de la unidad 2 terminada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
-// Tareas de Programacion Orientada a Objetos
+    public static void main(String[] args) {
+        /*
+        Clases de un Hospital
+        -Pacientes
+        -Medicos
+        -Consultas
+        Packetes Paciente clase paciente
+
+         */
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0;
+        while (opcion != 7) {
+            System.out.println("\n*** BIEVENIDO ***");
+            System.out.println("1.Registrar Paciente ");
+            System.out.println("2.Registrar Medico ");
+            System.out.println("3.Registrar Consulta ");
+            System.out.println("4.Registrar Consultorio ");
+            System.out.println("5. ");
+            System.out.println("6. ");
+            System.out.println("7. Salir");
+
+            System.out.println("Selecciona una opción: ");
+            opcion = scanner.nextInt();
+
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("\nSeleccionaste la opción para Registrar Paciente ");
+                    break;
+                case 2:
+                    System.out.println("\nSeleccionaste la opción para Registrar Medico ");
+
+                    break;
+                case 3:
+                    System.out.println("\nSeleccionaste la opción para Registrar Consulta ");
+                    break;
+                case 4:
+                    System.out.println("\nSeleccionaste la opción para Registrar Consultorio ");
+                    break;
+                case 5:
+                    System.out.println("\nSeleccionaste la opción para");
+                    break;
+                case 6:
+                    System.out.println("\nSeleccionaste la opción para");
+                    break;
+
+                case 7:
+                    System.out.println("Hasta luego");
+                    return;
+            }
+        }
+    }
 }
