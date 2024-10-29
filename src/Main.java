fb94adc35dfc7ef6a76dfa8d45b87d32cb618a5e
Nico9990 <nicolasguzman070@gmail.com>
2024-10-29 00:22:25 -0600

    tarea6/unidad3 terminada

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@
 public class Main {
     public static void main(String[] args) {
        Envio envioNacional=new EnvioNacional(300.00,6.0,600);
-
        envioNacional.calcularTiempoYCostoEntrega();
-        System.out.printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-       Envio envioInternacional=new EnvioInternacional(500,12.0,"España");
 
+        System.out.printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+
+       Envio envioInternacional=new EnvioInternacional(500,12.0,"España");
        envioInternacional.calcularTiempoYCostoEntrega();
 
     }
