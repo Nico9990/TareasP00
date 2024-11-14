1214c649085242b9d750e7e67728b990d79b628f
Nico9990 <nicolasguzman070@gmail.com>
2024-11-13 20:17:02 -0600

    termine tarea 1 unidad 6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-// Tareas de Programacion Orientada a Objetos
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.mostrarMenuTareas();
+    }
 }
